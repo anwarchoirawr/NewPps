e01a77691ab5f52f0dea6dc532c91f2e11280f7f
joko <jokosusilo321123@gmail.com>
2024-09-18 09:26:11 +0700

    merubah vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  base: '/NewPps/'
-})
+  // base: mode === 'production' ? '/NewPps/' : '/', // Gunakan base untuk GitHub Pages, default untuk Vercel
+}));
